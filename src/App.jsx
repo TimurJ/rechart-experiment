7d9da82d8843b34459a3c09c0e5e725573508ac6
Timur Jalilov <timurjalilov1@gmail.com>
2021-10-22 16:20:06 +0100

    added yesterdays prices and fixed dynamic line

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine } from "recharts"
-import { ResponsiveContainer } from "recharts/lib/component/ResponsiveContainer"
 import "./App.css"
 
 function App() {
@@ -8,59 +7,60 @@ function App() {
   const [yesterdayData, setYesterdayData] = useState()
 
   useEffect(() => {
-    fetch("https://sandbox.iexapis.com/stable/stock/aapl/intraday-prices/?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=15")
+    fetch("https://sandbox.iexapis.com/stable/stock/aapl/intraday-prices/?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=5")
       .then((response) => response.json())
       .then((data) => setLiveData(data))
   }, [])
 
   useEffect(() => {
-    fetch("https://sandbox.iexapis.com/stable/stock/aapl/chart/date/20211020?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=15")
+    fetch("https://sandbox.iexapis.com/stable/stock/aapl/chart/date/20211020?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=5")
       .then((response) => response.json())
       .then((data) => setYesterdayData(data))
   }, [])
 
   return (
     <div className="chart">
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart width={980} height={437} >
-          <CartesianGrid stroke="#eaebeb" verticalFill={["#f1f2f2", "#f9fafa"]} />
+      <LineChart width={980} height={437}>
+        <CartesianGrid stroke="#eaebeb" verticalFill={["#f1f2f2", "#f9fafa"]} />
 
-          <YAxis
-            stroke="#eaebeb"
-            tickSize={12}
-            allowDecimals={false}
-            interval="preserveStartEnd"
-            domain={["dataMin-2", "auto"]}
-            tick={{ fill: "#7f7f7f", fontSize: 12, fontFamily: "Roboto" }}
-            padding={{ top: 24 }}
-          />
+        <YAxis
+          stroke="#eaebeb"
+          tickSize={12}
+          tick={{ fill: "#7f7f7f", fontSize: 12, fontFamily: "Roboto" }}
+          interval="preserveStart"
+          // tickCount={12}
+          allowDecimals={false}
+          domain={["auto", "auto"]}
+          padding={{ top: 24 }}
+        />
 
-          <YAxis/>
+        <XAxis
+          stroke="#eaebeb"
+          tickSize={10}
+          tick={{ fill: "#7f7f7f", fontSize: 12, fontFamily: "Roboto" }}
+          interval="preserveStart"
+          allowDuplicatedCategory={false}
+          data={yesterdayData}
+          dataKey="label"
+        />
 
-          <XAxis
-            allowDuplicatedCategory={false}
-            stroke="#eaebeb"
-            tickSize={10}
-            interval="preserveStart"
-            data={yesterdayData}
-            dataKey="label"
-            tick={{ fill: "#7f7f7f", fontSize: 12, fontFamily: "Roboto" }}
-          />
+        <Tooltip contentStyle={{ fontSize: 14, borderColor: "#aaabd1", textAlign: "center", fontFamily: "Roboto" }} />
 
-          <Tooltip
-            contentStyle={{ borderColor: "#aaabd1", textAlign: "center" }}
-            labelStyle={{ fontSize: 14, fontFamily: "Roboto" }}
-            itemStyle={{ fontSize: 14, fontFamily: "Roboto" }}
-          />
+        <ReferenceLine y={155} stroke="#aaabd1" strokeDasharray="3 3" />
 
-          <ReferenceLine y={155} stroke="#aaabd1" strokeDasharray="3 3" />
+        <Line hide={false} name="OPEN" data={liveData} dataKey="open" stroke="#aaabd1" strokeWidth={2} dot={false} activeDot={{ r: 4 }} />
 
-          <Line name="Today" data={liveData} dataKey="open" stroke="#aaabd1" strokeWidth={2} dot={false} activeDot={{ r: 4 }} />
-
-          <Line name="Yesterday" data={yesterdayData}  dataKey='open' stroke="grey" strokeWidth={2} dot={false} activeDot={{ r: 4 }} />
-
-        </LineChart>
-      </ResponsiveContainer>
+        <Line
+          hide={true}
+          name="21ST OCTOBER OPEN"
+          data={yesterdayData}
+          dataKey="open"
+          stroke="grey"
+          strokeWidth={2}
+          dot={false}
+          activeDot={{ r: 4 }}
+        />
+      </LineChart>
     </div>
   )
 }
