fdb7ff2d37a42f467895fa4996b9ef402990f539
Timur Jalilov <timurjalilov1@gmail.com>
2021-10-22 18:09:22 +0100

    fixed the grid colors and horizontal lines not showing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,48 @@
 import React, { useEffect, useState } from "react"
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ReferenceLine } from "recharts"
+import { CartesianAxis } from "recharts/lib/cartesian/CartesianAxis"
 import "./App.css"
+
+// verticalFill={["#f1f2f2", "#f9fafa"]}
 
 function App() {
   const [liveData, setLiveData] = useState()
   const [yesterdayData, setYesterdayData] = useState()
+  const [yesterdayClose, setYesterdayClose] = useState()
 
   useEffect(() => {
-    fetch("https://sandbox.iexapis.com/stable/stock/aapl/intraday-prices/?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=5")
+    fetch("https://sandbox.iexapis.com/stable/stock/aapl/intraday-prices/?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=10")
       .then((response) => response.json())
       .then((data) => setLiveData(data))
   }, [])
 
   useEffect(() => {
-    fetch("https://sandbox.iexapis.com/stable/stock/aapl/chart/date/20211020?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=5")
+    fetch("https://sandbox.iexapis.com/stable/stock/aapl/chart/date/20211020?token=Tpk_095b8e5990924d0c8c41c2209556da53&chartInterval=10")
       .then((response) => response.json())
       .then((data) => setYesterdayData(data))
   }, [])
 
+  useEffect(() => {
+    fetch("https://sandbox.iexapis.com/stable/stock/aapl/previous/?token=Tpk_095b8e5990924d0c8c41c2209556da53")
+      .then((response) => response.json())
+      .then((data) => setYesterdayClose(data))
+  }, [])
+
   return (
     <div className="chart">
-      <LineChart width={980} height={437}>
-        <CartesianGrid stroke="#eaebeb" verticalFill={["#f1f2f2", "#f9fafa"]} />
+      <LineChart width={1000} height={477}>
+        <CartesianGrid stroke="#eaebeb" verticalFill={["#e6e6e680", "#ffffff00"]} />
 
         <YAxis
           stroke="#eaebeb"
-          tickSize={12}
+          tickSize={10}
           tick={{ fill: "#7f7f7f", fontSize: 12, fontFamily: "Roboto" }}
-          interval="preserveStart"
-          // tickCount={12}
+          interval="preserveStartEnd"
+          tickCount={12}
           allowDecimals={false}
           domain={["auto", "auto"]}
           padding={{ top: 24 }}
+          dx={-5}
         />
 
         <XAxis
@@ -42,11 +53,12 @@ function App() {
           allowDuplicatedCategory={false}
           data={yesterdayData}
           dataKey="label"
+          dy={5}
         />
 
         <Tooltip contentStyle={{ fontSize: 14, borderColor: "#aaabd1", textAlign: "center", fontFamily: "Roboto" }} />
 
-        <ReferenceLine y={155} stroke="#aaabd1" strokeDasharray="3 3" />
+        <ReferenceLine y={153} stroke="#aaabd1" strokeDasharray="5 3" />
 
         <Line hide={false} name="OPEN" data={liveData} dataKey="open" stroke="#aaabd1" strokeWidth={2} dot={false} activeDot={{ r: 4 }} />
 
